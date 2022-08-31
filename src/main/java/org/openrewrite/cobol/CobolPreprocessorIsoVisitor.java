573ad3e578420275f8147cecd4beea7b44d4322c
traceyyoshima <tracey.yoshima@gmail.com>
2022-08-31 10:14:16 -0700

    Added visits to CobolPreprocessorIsoVisitor. Added missing licenses.

--- src/main/java/org/openrewrite/cobol/CobolPreprocessorIsoVisitor.java
+++ src/main/java/org/openrewrite/cobol/CobolPreprocessorIsoVisitor.java
@@ -15,9 +15,95 @@
  */
 package org.openrewrite.cobol;
 
-import org.openrewrite.cobol.tree.Cobol;
-import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.cobol.tree.CobolPreprocessor;
 
 public class CobolPreprocessorIsoVisitor<P> extends CobolPreprocessorVisitor<P> {
 
+    public CobolPreprocessor.CharData visitCharData(CobolPreprocessor.CharData charData, P p) {
+        return (CobolPreprocessor.CharData) super.visitCharData(charData, p);
+    }
+
+    public CobolPreprocessor.CharDataLine visitCharDataLine(CobolPreprocessor.CharDataLine charDataLine, P p) {
+        return (CobolPreprocessor.CharDataLine) super.visitCharDataLine(charDataLine, p);
+    }
+
+    public CobolPreprocessor.CharDataSql visitCharDataSql(CobolPreprocessor.CharDataSql charDataSql, P p) {
+        return (CobolPreprocessor.CharDataSql) super.visitCharDataSql(charDataSql, p);
+    }
+
+    public CobolPreprocessor.CompilationUnit visitCompilationUnit(CobolPreprocessor.CompilationUnit compilationUnit, P p) {
+        return (CobolPreprocessor.CompilationUnit) super.visitCompilationUnit(compilationUnit, p);
+    }
+
+    public CobolPreprocessor.CompilerOption visitCompilerOption(CobolPreprocessor.CompilerOption compilerOption, P p) {
+        return (CobolPreprocessor.CompilerOption) super.visitCompilerOption(compilerOption, p);
+    }
+
+    public CobolPreprocessor.CompilerOptions visitCompilerOptions(CobolPreprocessor.CompilerOptions compilerOptions, P p) {
+        return (CobolPreprocessor.CompilerOptions) super.visitCompilerOptions(compilerOptions, p);
+    }
+
+    public CobolPreprocessor.CompilerXOpts visitCompilerXOpts(CobolPreprocessor.CompilerXOpts compilerXOpts, P p) {
+        return (CobolPreprocessor.CompilerXOpts) super.visitCompilerXOpts(compilerXOpts, p);
+    }
+
+    public CobolPreprocessor.CopySource visitCopySource(CobolPreprocessor.CopySource copySource, P p) {
+        return (CobolPreprocessor.CopySource) super.visitCopySource(copySource, p);
+    }
+
+    public CobolPreprocessor.CopyStatement visitCopyStatement(CobolPreprocessor.CopyStatement copyStatement, P p) {
+        return (CobolPreprocessor.CopyStatement) super.visitCopyStatement(copyStatement, p);
+    }
+
+    public CobolPreprocessor.DirectoryPhrase visitDirectoryPhrase(CobolPreprocessor.DirectoryPhrase directoryPhrase, P p) {
+        return (CobolPreprocessor.DirectoryPhrase) super.visitDirectoryPhrase(directoryPhrase, p);
+    }
+
+    public CobolPreprocessor.EjectStatement visitEjectStatement(CobolPreprocessor.EjectStatement ejectStatement, P p) {
+        return (CobolPreprocessor.EjectStatement) super.visitEjectStatement(ejectStatement, p);
+    }
+
+    public CobolPreprocessor.ExecStatement visitExecStatement(CobolPreprocessor.ExecStatement execStatement, P p) {
+        return (CobolPreprocessor.ExecStatement) super.visitExecStatement(execStatement, p);
+    }
+
+    public CobolPreprocessor.FamilyPhrase visitFamilyPhrase(CobolPreprocessor.FamilyPhrase familyPhrase, P p) {
+        return (CobolPreprocessor.FamilyPhrase) super.visitFamilyPhrase(familyPhrase, p);
+    }
+
+    public CobolPreprocessor.PseudoText visitPseudoText(CobolPreprocessor.PseudoText pseudoText, P p) {
+        return (CobolPreprocessor.PseudoText) super.visitPseudoText(pseudoText, p);
+    }
+
+    public CobolPreprocessor.ReplaceArea visitReplaceArea(CobolPreprocessor.ReplaceArea replaceArea, P p) {
+        return (CobolPreprocessor.ReplaceArea) super.visitReplaceArea(replaceArea, p);
+    }
+
+    public CobolPreprocessor.ReplaceByStatement visitReplaceByStatement(CobolPreprocessor.ReplaceByStatement replaceByStatement, P p) {
+        return (CobolPreprocessor.ReplaceByStatement) super.visitReplaceByStatement(replaceByStatement, p);
+    }
+
+    public CobolPreprocessor.ReplaceClause visitReplaceClause(CobolPreprocessor.ReplaceClause replaceClause, P p) {
+        return (CobolPreprocessor.ReplaceClause) super.visitReplaceClause(replaceClause, p);
+    }
+
+    public CobolPreprocessor.ReplaceOffStatement visitReplaceOffStatement(CobolPreprocessor.ReplaceOffStatement replaceOffStatement, P p) {
+        return (CobolPreprocessor.ReplaceOffStatement) super.visitReplaceOffStatement(replaceOffStatement, p);
+    }
+
+    public CobolPreprocessor.ReplacingPhrase visitReplacingPhrase(CobolPreprocessor.ReplacingPhrase replacingPhrase, P p) {
+        return (CobolPreprocessor.ReplacingPhrase) super.visitReplacingPhrase(replacingPhrase, p);
+    }
+
+    public CobolPreprocessor.SkipStatement visitSkipStatement(CobolPreprocessor.SkipStatement skipStatement, P p) {
+        return (CobolPreprocessor.SkipStatement) super.visitSkipStatement(skipStatement, p);
+    }
+
+    public CobolPreprocessor.TitleStatement visitTitleStatement(CobolPreprocessor.TitleStatement titleStatement, P p) {
+        return (CobolPreprocessor.TitleStatement) super.visitTitleStatement(titleStatement, p);
+    }
+
+    public CobolPreprocessor.Word visitWord(CobolPreprocessor.Word word, P p) {
+        return (CobolPreprocessor.Word) super.visitWord(word, p);
+    }
 }
