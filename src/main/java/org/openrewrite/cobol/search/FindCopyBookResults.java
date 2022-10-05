45eede5b5d3cd30d5bbcb4fa9fb6f9578c22e500
traceyyoshima <tracey.yoshima@gmail.com>
2022-10-05 16:20:58 -0700

    Allow FindCopyBookResults to match all copied sources if copyBookName is null

--- src/main/java/org/openrewrite/cobol/search/FindCopyBookResults.java
+++ src/main/java/org/openrewrite/cobol/search/FindCopyBookResults.java
@@ -6,6 +6,7 @@ import org.openrewrite.*;
 import org.openrewrite.cobol.CobolIsoVisitor;
 import org.openrewrite.cobol.tree.Cobol;
 import org.openrewrite.cobol.tree.Copy;
+import org.openrewrite.internal.lang.Nullable;
 
 @EqualsAndHashCode(callSuper = true)
 @Value
@@ -13,7 +14,9 @@ public class FindCopyBookResults extends Recipe {
 
     @Option(displayName = "Copy book name",
             description = "The copy book name to search for.",
-            example = "KP008")
+            example = "KP008",
+            required = false)
+    @Nullable
     String copyBookName;
 
     @Override
@@ -23,12 +26,12 @@ public class FindCopyBookResults extends Recipe {
 
     @Override
     public String getDescription() {
-        return "Search for a copy book by name and find the produced COBOL code after preprocessing.";
+        return "Search for a copy book by name and find the produced COBOL code after preprocessing. The recipe will mark all copied sources if a book name is not provided.";
     }
 
     @Override
     protected TreeVisitor<?, ExecutionContext> getSingleSourceApplicableTest() {
-        return new UsesCopyBook(copyBookName);
+        return copyBookName == null ? null : new UsesCopyBook(copyBookName);
     }
 
     @Override
@@ -39,7 +42,7 @@ public class FindCopyBookResults extends Recipe {
     private static class MarkCopyBook extends CobolIsoVisitor<ExecutionContext> {
         private final String bookName;
 
-        public MarkCopyBook(String bookName) {
+        public MarkCopyBook(@Nullable String bookName) {
             this.bookName = bookName;
         }
 
@@ -47,7 +50,7 @@ public class FindCopyBookResults extends Recipe {
         public Cobol.Word visitWord(Cobol.Word word, ExecutionContext executionContext) {
             Cobol.Word w = super.visitWord(word, executionContext);
             Copy copy = w.getMarkers().findFirst(Copy.class).orElse(null);
-            if (copy != null && bookName.equals(copy.getOriginalStatement().getCopySource().getName().getWord())) {
+            if (copy != null && (bookName == null || bookName.equals(copy.getOriginalStatement().getCopySource().getName().getWord()))) {
                 boolean isMarked = w.getMarkers().getMarkers().stream()
                         .anyMatch(it -> it instanceof SearchResult && ((SearchResult) it).getType() == SearchResult.Type.COPIED_SOURCE);
 
