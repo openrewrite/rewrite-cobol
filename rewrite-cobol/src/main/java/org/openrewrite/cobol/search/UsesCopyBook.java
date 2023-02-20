f92c7e5ed3f253dbc75b95e88a99204060695faf
traceyyoshima <tracey.yoshima@gmail.com>
2023-02-20 12:59:52 -0800

    Updated UsesCopyBook to check Cobol$Word$CopyStatement instead of Copy marker.

--- rewrite-cobol/src/main/java/org/openrewrite/cobol/search/UsesCopyBook.java
+++ rewrite-cobol/src/main/java/org/openrewrite/cobol/search/UsesCopyBook.java
@@ -5,8 +5,6 @@ import lombok.Value;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.cobol.CobolIsoVisitor;
 import org.openrewrite.cobol.tree.Cobol;
-import org.openrewrite.cobol.tree.CobolPreprocessor;
-import org.openrewrite.cobol.markers.Copy;
 import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.marker.SearchResult;
 
@@ -26,7 +24,7 @@ public class UsesCopyBook extends CobolIsoVisitor<ExecutionContext> {
     @Override
     public Cobol.CompilationUnit visitCompilationUnit(Cobol.CompilationUnit compilationUnit, ExecutionContext executionContext) {
         Cobol.CompilationUnit cu = compilationUnit;
-        if (FindCopyBook.find(cu, copyBookName) != null) {
+        if (FindCopySource.find(cu, copyBookName) != null) {
             cu = SearchResult.found(cu);
         }
         return cu;
@@ -35,49 +33,21 @@ public class UsesCopyBook extends CobolIsoVisitor<ExecutionContext> {
     private static class FindCopySource extends CobolIsoVisitor<ExecutionContext> {
 
         @Nullable
-        public static CobolPreprocessor.CopySource find(Cobol cobol, String bookName) {
-            CobolIsoVisitor<List<CobolPreprocessor.CopySource>> visitor = new CobolIsoVisitor<List<CobolPreprocessor.CopySource>>() {
+        public static Cobol.Preprocessor.CopySource find(Cobol cobol, String bookName) {
+            CobolIsoVisitor<List<Cobol.Preprocessor.CopySource>> visitor = new CobolIsoVisitor<List<Cobol.Preprocessor.CopySource>>() {
                 @Override
-                public Cobol.Word visitWord(Cobol.Word word, List<CobolPreprocessor.CopySource> copySources) {
+                public Cobol.Word visitWord(Cobol.Word word, List<Cobol.Preprocessor.CopySource> copySources) {
                     Cobol.Word w = super.visitWord(word, copySources);
                     if (copySources.isEmpty()) {
-                        Copy copy = w.getMarkers().findFirst(Copy.class).orElse(null);
-                        if (copy != null && bookName.equals(copy.getOriginalStatement().getCopySource().getName().getWord())) {
-                            copySources.add(copy.getOriginalStatement().getCopySource());
+                        if (w.getCopyStatement() != null && bookName.equals(w.getCopyStatement().getCopySource().getName().getWord())) {
+                            copySources.add(w.getCopyStatement().getCopySource());
                         }
                     }
                     return w;
                 }
             };
 
-            List<CobolPreprocessor.CopySource> result = new ArrayList<>(1);
-            visitor.visit(cobol, result);
-            return result.isEmpty() ? null : result.get(0);
-        }
-    }
-
-    private static class FindCopyBook extends CobolIsoVisitor<ExecutionContext> {
-
-        @Nullable
-        public static CobolPreprocessor.CopyBook find(Cobol cobol, String bookName) {
-            CobolIsoVisitor<List<CobolPreprocessor.CopyBook>> visitor = new CobolIsoVisitor<List<CobolPreprocessor.CopyBook>>() {
-                @Override
-                public Cobol.Word visitWord(Cobol.Word word, List<CobolPreprocessor.CopyBook> copyBooks) {
-                    Cobol.Word w = super.visitWord(word, copyBooks);
-                    if (copyBooks.isEmpty()) {
-                        Copy copy = w.getMarkers().findFirst(Copy.class).orElse(null);
-                        if (copy != null &&
-                                bookName.equals(copy.getOriginalStatement().getCopySource().getName().getWord()) &&
-                                copy.getOriginalStatement().getCopyBook() != null) {
-
-                            copyBooks.add(copy.getOriginalStatement().getCopyBook());
-                        }
-                    }
-                    return w;
-                }
-            };
-
-            List<CobolPreprocessor.CopyBook> result = new ArrayList<>(1);
+            List<Cobol.Preprocessor.CopySource> result = new ArrayList<>(1);
             visitor.visit(cobol, result);
             return result.isEmpty() ? null : result.get(0);
         }
