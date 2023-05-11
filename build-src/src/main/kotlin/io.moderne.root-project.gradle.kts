823d13a22f3ff2badc82a7c19bd6405efe382cb7
Sam Snyder <sam@moderne.io>
2023-05-11 14:53:00 -0700

    Try putting this back in to see if we can successfully publish to nexus snapshots

--- build-src/src/main/kotlin/io.moderne.root-project.gradle.kts
+++ build-src/src/main/kotlin/io.moderne.root-project.gradle.kts
@@ -4,12 +4,11 @@ plugins {
     id("io.github.gradle-nexus.publish-plugin")
 }
 
-// An interaction with the GCP artifact registry plugin causes a problem with this during release
-//nexusPublishing {
-//    repositories {
-//        sonatype()
-//    }
-//}
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
+}
 
 configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
     defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
