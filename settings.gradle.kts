510c6284e31960a6ab8d2c398c816d54fcde61ff
Sam Snyder <sam@moderne.io>
2023-05-11 21:20:19 -0700

    Temporarily disable nist LST publishing project so it doesn't interfere with publishing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,8 @@ enableFeaturePreview("VERSION_ORDERING_V2")
 includeBuild("build-src")
 
 include("rewrite-cobol")
-include("publish-nist-asts")
-include("publish-nist-asts:src:main:test")
+//include("publish-nist-asts")
+//include("publish-nist-asts:src:main:test")
 include("rewrite-jcl")
 include("cobol-analysis")
-findProject(":publish-nist-asts:src:main:test")?.name = "test"
+//findProject(":publish-nist-asts:src:main:test")?.name = "test"
