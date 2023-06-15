b643db237bd301a5f6954a435cd44ae8a8f80a81
Kun Li <kun@moderne.io>
2023-06-15 13:48:16 -0700

    Add license headers to pass compile

--- rewrite-cobol/src/test/java/org/openrewrite/cobol/tree/cobol/search/FindWordTest.java
+++ rewrite-cobol/src/test/java/org/openrewrite/cobol/tree/cobol/search/FindWordTest.java
@@ -1,3 +1,8 @@
+/*
+ * For commercial customers of Moderne Inc., this repository is licensed per the terms of our contract.
+ * For everyone else, this is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International.
+ * See: https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
 package org.openrewrite.cobol.tree.cobol.search;
 
 import org.openrewrite.cobol.CobolTest;
