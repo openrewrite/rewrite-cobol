61ad584b3de4f2c297c28686ec107cebf4047520
Kun Li <kun@moderne.io>
2023-06-15 20:55:07 -0700

    translate CobolPreprocessorCopyTest to java. but need to fix failing tests

--- rewrite-cobol/src/test/java/org/openrewrite/cobol/tree/preprocessor/CobolPreprocessorCopyTest.java
+++ rewrite-cobol/src/test/java/org/openrewrite/cobol/tree/preprocessor/CobolPreprocessorCopyTest.java
@@ -5,7 +5,134 @@
  */
 package org.openrewrite.cobol.tree.preprocessor;
 
+import org.junit.jupiter.api.Test;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.PrintOutputCapture;
+import org.openrewrite.cobol.CobolParser;
+import org.openrewrite.cobol.CobolPreprocessorVisitor;
 import org.openrewrite.cobol.CobolTest;
+import org.openrewrite.cobol.internal.CobolDialect;
+import org.openrewrite.cobol.internal.CobolPreprocessorOutputSourcePrinter;
+import org.openrewrite.cobol.internal.CobolPreprocessorPrinter;
+import org.openrewrite.cobol.tree.CobolPreprocessor;
+import org.openrewrite.cobol.tree.Space;
+import org.openrewrite.internal.EncodingDetectingInputStream;
+import org.openrewrite.test.RecipeSpec;
 
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.openrewrite.cobol.Assertions.preprocessor;
+import static org.openrewrite.test.RewriteTest.toRecipe;
+
+// TODO: Fix failing tests
 public class CobolPreprocessorCopyTest extends CobolTest {
+    private static final CobolDialect DIALECT = CobolDialect.ibmAnsi85();
+    public static CobolPreprocessorPrinter<ExecutionContext> printer = new CobolPreprocessorPrinter<>(false, true);
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec
+          // .parser(CobolParser.builder().setEnableCopy(false).setEnableReplace(false).setCobolDialect(CobolDialect.hpTandem()))
+          .recipe(toRecipe(context -> new CobolPreprocessorVisitor<ExecutionContext>() {
+            @Override
+            public Space visitSpace(Space space, Space.Location location, ExecutionContext p) {
+                String whitespace = space.getWhitespace().trim();
+                if (!(DIALECT.getSeparators().contains(whitespace + " ") || whitespace.isEmpty())) {
+                    return space.withWhitespace("(~~>" + space.getWhitespace() + "<~~)");
+                }
+                return space;
+            }
+
+            @Override
+            public CobolPreprocessor visitCopyStatement(CobolPreprocessor.CopyStatement copyStatement, ExecutionContext p) {
+                assertThat(copyStatement.getCopyBook()).isNotNull();
+                assertThat(copyStatement.getCopyBook().getSourcePath()).isNotNull();
+                CobolPreprocessor.CopyBook copyBook = copyStatement.getCopyBook();
+
+                PrintOutputCapture<ExecutionContext> output = new PrintOutputCapture<>(new InMemoryExecutionContext());
+                CobolPreprocessorOutputSourcePrinter<ExecutionContext> printer =
+                  new CobolPreprocessorOutputSourcePrinter<>(CobolDialect.ibmAnsi85(), true);
+                printer.visit(copyBook, output);
+
+                String source = getSource(copyBook.getSourcePath());
+                assertThat(source).isEqualTo(output.getOut());
+
+                return super.visitCopyStatement(copyStatement, p);
+            }
+        }));
+    }
+
+    private String getSource(Path copyBook) {
+        String source;
+        try {
+            try (InputStream inputStream = Files.newInputStream(copyBook)) {
+                EncodingDetectingInputStream input = new EncodingDetectingInputStream(inputStream);
+                source = input.readFully();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return source;
+    }
+
+    @Test
+    void sm101A() {
+        rewriteRun(
+          preprocessor(getNistResource("SM101A.CBL"))
+        );
+    }
+
+    @Test
+    void sm103A() {
+        rewriteRun(
+      preprocessor(getNistResource("SM103A.CBL"))
+        );
+    }
+
+    @Test
+    void sm105A() {
+        rewriteRun(
+      preprocessor(getNistResource("SM105A.CBL"))
+        );
+    }
+
+    @Test
+    void sm106A() {
+        rewriteRun(
+      preprocessor(getNistResource("SM106A.CBL"))
+        );
+    }
+
+    @Test
+    void sm107A() {
+        rewriteRun(
+      preprocessor(getNistResource("SM107A.CBL"))
+        );
+    }
+
+    @Test
+    void sm207A() {
+        rewriteRun(
+      preprocessor(getNistResource("SM207A.CBL"))
+        );
+    }
+
+    @Test
+    void sm301M() {
+        rewriteRun(
+      preprocessor(getNistResource("SM301M.CBL"))
+        );
+    }
+
+    @Test
+    void sm401M() {
+        rewriteRun(
+          preprocessor(getNistResource("SM401M.CBL"))
+        );
+    }
 }
